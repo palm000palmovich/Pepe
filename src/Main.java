ecc9f7cf23584720be229e4734873282dcb8b598
palm000palmovich <ivankiselev6534@gmail.com>
2024-06-26 10:45:11 +0300

    ex1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        byte clientOS = 1;
+        if (clientOS != 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+
+        }
+}
