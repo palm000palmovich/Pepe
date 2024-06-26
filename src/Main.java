256ed92316bf625b6c25e7aa70830a680b8a3be5
palm000palmovich <ivankiselev6534@gmail.com>
2024-06-26 12:10:41 +0300

    ex5

--- src/Main.java
+++ src/Main.java
@@ -57,6 +57,52 @@ public class Main {
             System.out.println("На такие большие расстояния не доставляем.");
         }
 
+        for (int i = 0; i < 2; i++)
+            System.out.println("\n");
+        System.out.println("Задание 5");
+        int monthNumber = 13;
+        if (monthNumber > 12 || monthNumber < 1){
+            System.out.println("Такого месяца нет!");
+        } else if (monthNumber <= 12 && monthNumber >= 1){
+            switch (monthNumber){
+                case 1:
+                    System.out.println("Январь");
+                    break;
+                case 2:
+                    System.out.println("Февраль");
+                    break;
+                case 3:
+                    System.out.println("Март");
+                    break;
+                case 4:
+                    System.out.println("Апрель");
+                    break;
+                case 5:
+                    System.out.println("Май");
+                    break;
+                case 6:
+                    System.out.println("Июнь");
+                    break;
+                case 7:
+                    System.out.println("Июль");
+                    break;
+                case 8:
+                    System.out.println("Август");
+                    break;
+                case 9:
+                    System.out.println("Сентябрь");
+                    break;
+                case 10:
+                    System.out.println("Октябрь");
+                    break;
+                case 11:
+                    System.out.println("Ноябрь");
+                    break;
+                case 12:
+                    System.out.println("Декабрь");
+                    break;
+            }
+        }
         }
 
 
